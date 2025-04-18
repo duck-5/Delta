a405612814e669ec48c99cffb6d1395f73d70d06
JesusDMan <mantin.work@gmail.com>
2025-04-18 22:57:58 +0300

    added cython to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     long_description=open("README.md").read(),
     url="https://github.com/JesusDMan/BackupSyncer",
     author="JesusDMan",
+    install_requires = ["cython"],
     ext_modules=cythonize("smart_delta/src/delta_utils.py"),
     entry_points={
         "console_scripts": [
